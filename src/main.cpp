9257685aa66d5874ea332c86d401c2e735e21bac
ambiennt <ahook701@gmail.com>
2021-11-11 08:54:28 -0800

    map immobility status to player xuid

--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,79 @@
-#include <dllentry.h>
 #include "main.h"
+#include <dllentry.h>
 
-std::unordered_set<std::string> shouldResendImmobilityStatus;
+std::unordered_set<int64_t> shouldResendImmobilityStatus;
 
 class ImmobileCommand : public Command {
 public:
 
-  ImmobileCommand() { selector.setIncludeDeadPlayers(true); }
+	ImmobileCommand() { selector.setIncludeDeadPlayers(true); }
 
-  //initialize command arguments
-  CommandSelector<Player> selector;
-  bool toggle = true;
+	// initialize command arguments
+	CommandSelector<Player> selector;
+	bool toggle = true;
 
-  void setImmobilityStatus(Player *player, bool toggle) {
-    CallServerClassMethod<bool>("?setStatusFlag@Actor@@QEAA_NW4ActorFlags@@_N@Z", player, ActorFlags::IMMOBILE, toggle);
-    std::string immobilityStatusString = toggle ? "You have been immobilized" : "You have been mobilized";
-    auto pkt = TextPacket::createTextPacket<TextPacketType::SystemMessage>(immobilityStatusString);
-    player->sendNetworkPacket(pkt);
+	void setImmobilityStatus(Player *player, CommandOutput &output, bool sendCommandFeedback) {
+		player->setStatusFlag(ActorFlags::Immobile, toggle);
+		std::string immobilityStatusString = toggle ? "You have been immobilized" : "You have been mobilized";
 
-    bool isImmobileInMap = shouldResendImmobilityStatus.count(player->getEntityName());
-    if (toggle) {
-      if (!isImmobileInMap) {
-        shouldResendImmobilityStatus.insert(player->getEntityName());
-      }
-    }
-    else if (isImmobileInMap) {
-      shouldResendImmobilityStatus.erase(player->getEntityName());
-    }
-  }
+		if (sendCommandFeedback) {
+			auto output = TextPacket::createTextPacket<TextPacketType::SystemMessage>(immobilityStatusString);
+			player->sendNetworkPacket(output);
+		}
 
-  void execute(CommandOrigin const &origin, CommandOutput &output) {
+		auto &db = Mod::PlayerDatabase::GetInstance();
+		auto xuid = db.Find(player)->xuid; // map immobility status to player xuid
 
-    //get selected entity/entities
-    auto selectedEntities = selector.results(origin);
-    if (selectedEntities.empty()) {
-      output.error("No targets matched selector");
-      return;
-    }
+		bool isImmobileInMap = shouldResendImmobilityStatus.count(xuid);
+		if (toggle && !isImmobileInMap) {
+			shouldResendImmobilityStatus.insert(xuid);
+		}
+		else if (isImmobileInMap) {
+			shouldResendImmobilityStatus.erase(xuid);
+		}
+	}
 
-    for (auto player : selectedEntities) {
-      setImmobilityStatus(player, toggle);
-    }
-    
-    if (toggle) {
-      output.success("Immobilized " + std::to_string(selectedEntities.count()) + (selectedEntities.count() == 1 ? " player" : " players"));
-    }
-    else {
-      output.success("Mobilized " + std::to_string(selectedEntities.count()) + (selectedEntities.count() == 1 ? " player" : " players"));
-    }
-  }
+	void execute(CommandOrigin const &origin, CommandOutput &output) {
 
-  static void setup(CommandRegistry *registry) {
-    using namespace commands;
-    registry->registerCommand(
-      "immobile", "Locks player movement.", CommandPermissionLevel::GameMasters, CommandFlagCheat, CommandFlagNone);
-    registry->registerOverload<ImmobileCommand>("immobile",
-      mandatory(&ImmobileCommand::selector, "target"),
-      optional(&ImmobileCommand::toggle, "toggle")
-    );
-  }
+		//get selected entity/entities
+		auto selectedEntities = selector.results(origin);
+		if (selectedEntities.empty()) {
+			return output.error("No targets matched selector");
+		}
+
+		auto gr = CallServerClassMethod<GameRules*>("?getGameRules@Level@@QEAAAEAVGameRules@@XZ", LocateService<Level>());
+		GameRuleIds sendCommandFeedbackId = GameRuleIds::SendCommandFeedback;
+		bool sendCommandFeedback = CallServerClassMethod<bool>("?getBool@GameRules@@QEBA_NUGameRuleId@@@Z", gr, &sendCommandFeedbackId);
+
+		for (auto player : selectedEntities) {
+			setImmobilityStatus(player, output, sendCommandFeedback);
+		}
+		
+		if (toggle) {
+			output.success(
+				"Immobilized " + std::to_string(selectedEntities.count()) + std::string(selectedEntities.count() == 1 ? " player" : " players"));
+		}
+		else {
+			output.success(
+				"Mobilized " + std::to_string(selectedEntities.count()) + std::string(selectedEntities.count() == 1 ? " player" : " players"));
+		}
+	}
+
+	static void setup(CommandRegistry *registry) {
+		using namespace commands;
+		registry->registerCommand(
+			"immobile", "Locks player movement.", CommandPermissionLevel::GameMasters, CommandFlagNone, CommandFlagNone);
+		registry->registerOverload<ImmobileCommand>("immobile",
+			optional(&ImmobileCommand::selector, "target"),
+			optional(&ImmobileCommand::toggle, "toggle")
+		);
+	}
 };
 
-//new method allows commands to work in function files
 TClasslessInstanceHook(void, "?load@FunctionManager@@QEAAXAEAVResourcePackManager@@AEAVCommandRegistry@@@Z",
-  class ResourcePackManager &rpManager, class CommandRegistry &registry) {
-    ImmobileCommand::setup(&registry);
-    original(this, rpManager, registry);
+	class ResourcePackManager &rpManager, class CommandRegistry &registry) {
+		ImmobileCommand::setup(&registry);
+		original(this, rpManager, registry);
 }
 
 void dllenter() {}
@@ -74,11 +82,10 @@ void dllexit() {}
 //by default, immobile flag clears when a player leaves the world
 //hack: resend flag if immobile player leaves and rejoins
 void PreInit() {
-  Mod::PlayerDatabase::GetInstance().AddListener(SIG("joined"), [](Mod::PlayerEntry const &entry) {
-    if (shouldResendImmobilityStatus.count(entry.player->getEntityName())) {
-      ImmobileCommand ic;
-      ic.setImmobilityStatus(entry.player, true);
-    }
-  });
+	Mod::PlayerDatabase::GetInstance().AddListener(SIG("joined"), [](Mod::PlayerEntry const &entry) {
+		if (shouldResendImmobilityStatus.count(entry.xuid)) {
+			entry.player->setStatusFlag(ActorFlags::Immobile, true);
+		}
+	});
 }
 void PostInit() {}
--- src/main.h
+++ src/main.h
@@ -1,10 +1,15 @@
 #pragma once
 
+#include <hook.h>
+#include <base/base.h>
 #include <base/log.h>
 #include <mods/CommandSupport.h>
 #include <base/playerdb.h>
 #include <Actor/Actor.h>
 #include <Actor/Player.h>
+#include <Actor/ActorUniqueID.h>
+#include <Level/Level.h>
+#include <Level/GameRuleIds.h>
 #include <Packet/TextPacket.h>
 
 DEF_LOGGER("ImmobileCommand");
